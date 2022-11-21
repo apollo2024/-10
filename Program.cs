52846212084c816d4c8ea7c81082266766dcd9c7
muhammadkodir <muhammadkodyra@email.com>
2022-11-22 00:47:40 +0500

    the last one

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("number:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 999 || number < 100)
+{
+    Console.WriteLine("число не трехзначное");
+}
+else
+{
+int sum = (number / 10) - (number / 100) * 10 ;
+Console.WriteLine(sum);
+}
